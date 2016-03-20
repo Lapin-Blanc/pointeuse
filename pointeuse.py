bd1a1f99a6ae0c607f3f1822a01c1d06c8dd9ec1
Lapin-Blanc <fabien.toune@gmail.com>
2016-03-20 09:34:46 +0100

    Include LCD display in project

--- pointeuse.py
+++ pointeuse.py
@@ -4,18 +4,38 @@ import os
 import django
 from datetime import timedelta
 from datetime import datetime
+from time import sleep
 
 from django.utils import timezone
 from django.core.exceptions import ObjectDoesNotExist
 os.environ.setdefault("DJANGO_SETTINGS_MODULE","pointeuse.settings")
 django.setup()
+
+from RPi import GPIO as gpio
+gpio.setmode(gpio.BCM)
+from Adafruit_CharLCD import Adafruit_CharLCD as LCD
+
 from pointages.models import Personne, Pointage
 
 from pythonbeid.beid import BeidReader
 
 from pprint import pprint
+
 # Constants
 MIN_DELAY = 1
+
+GREEN_LED = 20
+RED_LED = 21
+
+gpio.setup(GREEN_LED, gpio.OUT)
+gpio.setup(RED_LED, gpio.OUT)
+
+gpio.output(RED_LED, True)
+gpio.output(GREEN_LED, False)
+
+lcd = LCD()
+lcd.clear()
+lcd.message("System ready\nInsert card...")
 
 class PointeuseReader(BeidReader):
 
@@ -23,13 +43,25 @@ class PointeuseReader(BeidReader):
         print("---------------------")
         print("Card removed")
         print("---------------------")
+        gpio.output(GREEN_LED, False)
+        gpio.output(RED_LED, False)
+        lcd.clear()
+        lcd.message("System ready\nInsert card...")
+
 
     def on_inserted(self, card):
+        gpio.output(RED_LED, True)
+        lcd.clear()
+        lcd.message("Reading infos...")
+
         infos = card.read_infos()
         print("---------------------")
         print("Card inserted")
         print("---------------------")
         pprint(infos)
+
+        gpio.output(GREEN_LED, True)
+        gpio.output(RED_LED, False)
 
         try:
             Personne.objects.get(num_nat=infos["num_nat"])
@@ -47,10 +79,14 @@ class PointeuseReader(BeidReader):
 
         p = Personne.objects.get(num_nat=infos["num_nat"])
         print(p)
+        p_name = p.nom + " " + p.prenoms.split()[0]
+        p_message = datetime.now().strftime("%H:%M") 
 
         if not p.pointage_set.filter(checkin__date = timezone.now().date()):
             p.pointage_set.create(checkin=timezone.now())
             print("Premier pointage de la journée")
+            p_message = p_message + " CHECKIN"
+            
         else:
             try:
                 pointage_started = p.pointage_set.get(checkin__date = timezone.now().date() , checkout__exact = None)
@@ -60,13 +96,19 @@ class PointeuseReader(BeidReader):
                     pointage_started.checkout = timezone.now()
                     pointage_started.save()
                     print("Pointage clôturé")
+                    p_message = p_message + " CHECKOUT"
             except ObjectDoesNotExist:
                 p.pointage_set.create(checkin=timezone.now())
                 print("Nouveau pointage pour la journée")
-
+                p_message = p_message + " CHECKIN"
+        ############################
+        lcd.clear()
+        lcd.message(p_name + "\n" + p_message)
+        ############################
+        
 if __name__ == "__main__":
     p = PointeuseReader()
     print("Application started")
 
     while True:
-        pass
+        sleep(3600)
